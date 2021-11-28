8caa7faf1d668f7b09e37e294ff6df62a2dbe726
AnhDW <vungocanh723@gmail.com>
2021-11-28 10:05:35 +0700

    upload

---
+++ nav.php
@@ -0,0 +1,56 @@
+	<nav>
+		<div id="dau">
+			<div class="logo"><a href="yamaha.html"><img src="img/logo-yamaha.png.webp"></a></div>
+			<div class="topnav">
+				<ul class="tren">
+					<li><a href="#">VỀ YAMAHA</a></li>
+					<li><a href="#">TIN TỨC</a></li>
+					<li><a href="#">TUYỂN DỤNG</a></li>
+					<li><a href="#">BẢNG GIÁ XE MÁY</a></li>
+					<li><a href="#">BÁN HÀNG DOANH NGHIỆP</a></li>
+					<li class="search-box">
+						<input type="text" class="search-txt" placeholder="Tìm kiếm...">
+						<a href="#" class="search-btn"><i class="fas fa-search"></i></a>
+					</li>
+				</ul>
+				<ul class="duoi">
+					<li><a href="#">NEW ME DISCOVER</a>
+						<ul class="submenu">
+							<li><p align="center">Scooter World</p><a href="#"><img class="hinh1" src="img/121554729_3402811596438542_242540317680922263_o.jpg"></a></li>
+							<li><p align="center">Sport World</p><a href="#"><img class="hinh1" src="img/121706524_3402812239771811_8068717623908904420_o.jpg"></a></li>
+						</ul>
+					</li>
+					<li><a href="#">SẢN PHẨM</a>
+						<ul class="submenu">
+							<li class="xe"><a href="xeso.html"><p align="center">Xe số</p></a>
+								<a href="#" class="hinh2"><img src="img/xeso/Exciter-Monster-150x120.png.webp"><p>Exciter</p></a>
+								<a href="#" class="hinh2"><img src="img/xeso/Jupiter-GP-150x120.png.webp"><p>Jupiter</p></a>
+								<a href="#" class="hinh2"><img src="img/xeso/Sirius-Mat-Black-RC-004-150x120.png.webp"><p>Sirius FI</p></a>
+								<a href="#" class="hinh2"><img src="img/xeso/902cbf7c3925180d16ad89df59f9d225-150x150.png.webp"><p>Sirius</p></a>
+							</li>
+							<li class="xe"><a href="xetayga.html"><p align="center">Xe tay ga</p></a>
+								<a href="#" class="hinh2"><img src="img/xetayga/Aruzo-150x120.png.webp"><p>Aruzo</p></a>
+								<a href="#" class="hinh2"><img src="img/xetayga/FreeGo-S-Mat-Blue-004-150x120.png.webp"><p>FreeGo</p></a>
+								<a href="#" class="hinh2"><img src="img/xetayga/Grande-Pearl-White-004-150x120.png.webp"><p>Grande</p></a>
+								<a href="#" class="hinh2"><img src="img/xetayga/Janus-White-Limited-004-1-150x120.png.webp"><p>Janus</p></a>
+								<a href="#" class="hinh2"><img src="img/xetayga/Latte-Yamaha-150x120.png.webp"><p>Latte</p></a>
+								<a href="#" class="hinh2"><img src="img/xetayga/NVX-cam-150x150.png.webp"><p>NVX</p></a>
+							</li>
+							<li class="xe"><a href="xethethao.html"><p align="center">Xe thể thao</p></a>
+								<a href="#" class="hinh2"><img src="img/xethethao/MT-03-150x120.png.webp"><p>MT-03</p></a>
+								<a href="#" class="hinh2"><img src="img/xethethao/MT-15-GP-004-1-150x120.png.webp"><p>MT-15</p></a>
+								<a href="#" class="hinh2"><img src="img/xethethao/TFX150-150x120.png.webp"><p>TFX</p></a>
+								<a href="#" class="hinh2"><img src="img/xethethao/YZF-R15-150x120.png.webp"><p>YZF-R15</p></a>
+								<a href="#" class="hinh2"><img src="img/xethethao/YZF-R3-150x120.png.webp"><p>YZF-R3</p></a>
+							</li>
+						</ul>
+					</li>
+					<li><a href="#">CÔNG NGHỆ</a></li>
+					<li><a href="#">DỊCH VỤ</a></li>
+					<li><a href="#">PHỤ TÙNG</a></li>
+					<li><a href="#">KHUYẾN MÃI</a></li>
+					<li><a href="#">ĐẠI LÝ</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
